4ad0d9007cb62a625cc3cc3edffe9d967493e125
Niklas Engvall <niklas_engvall@hotmail.com>
2022-04-24 18:34:39 +0200

    Added an example

--- BookI/Chapter04/AddInteger2.cpp
+++ BookI/Chapter04/AddInteger2.cpp
@@ -1,17 +1,41 @@
+// Adding variable value to another variable with the same type and store the result in a new variable
+
+// Original file:   main.cpp
+// Original folder: CPP_AIO4/BookI/Chapter04/AddInteger2
+
+// Code from:       C++ All in one for dummies, 4th edition, 2021, author John Paul Mueller
+//                  Book I, chapter 4, p. 79
+
+// File:            AddInteger2.cpp 
+// Created:         2022-04-24, Niklas Engvall
+// Updated:         2022-04-24, Niklas Engvall
+
+// Purpose:         This example shows how to store calculations between variables in a third variable with the same type
+
+// Description:     The code in main declares variables then assigns some values to start_time and end_time
+//                  After that we calculate the total_time and elapsed_time
+//                  The last thing we do is printing the results using the variables that stores the result of the calculations
+
+// Returns:         0 to calling function, software or operating system#include <iostream>
+
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
-int main()
+int main(void)
 {
-    int start;
-    int time;
-    int total;
-
-    start = 37;
-    time = 22;
-    total =  start + time;
-
-    cout << total << endl;
+    int start_time, end_time, total_time, elapsed_time; // Variable declarations
+    
+    start_time = 25; // Variable initilizing
+    end_time = 55;
+    
+    total_time =  start_time + end_time;    // Make some calulations
+    elapsed_time = end_time - start_time; 
+    
+    // Prints result 
+    cout << "Total time: " << total_time << endl;                                           
+    cout << "Time elapsed between start_time and end_time: " <<  elapsed_time << endl;    
+        
     return 0;
 }
--- BookI/Chapter04/AddInteger3.cpp
+++ BookI/Chapter04/AddInteger3.cpp
@@ -1,15 +1,44 @@
+// Adding values to a variable and store it in the same variable 
+
+// Original file:   main.cpp
+// Original folder: CPP_AIO4/BookI/Chapter04/AddInteger3
+
+// Code from:       C++ All in one for dummies, 4th edition, 2021, author John Paul Mueller
+//                  Book I, chapter 4, p. 80-81
+
+// File:            AddInteger3.cpp 
+// Created:         2022-04-24, Niklas Engvall
+// Updated:         2022-04-24, Niklas Engvall
+
+// Purpose:         This example shows how to store calculations into the very same variable
+
+// Description:     The code in main declares and initialize a variable var to 7
+//                  After that we print its value on the console
+//                  Then it adds 7 to the var value and which is 14 and stores that new value in var.
+//                  The new value is printed on the console
+//                  The program continues and shows how to add new values in different ways using += and ++ operators and prints it on screen
+
+// Returns:         0 to calling function, software or operating system#include <iostream>#include <iostream>
+
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 int main()
 {
-    int total;
-    total = 12;
-    cout << total << endl;
+    int var = 7;
 
-    total = total + 5;
-    cout << total << endl;
+    cout << var << endl; // Prints the var value (7)
+
+    var = var + 7;          // Adds 7 to the variable and assign the new value (14) into the same variable
+    cout << var << endl;
+
+    var += 14;              // Same as above but written in a shorter way, var now stores the value 28
+    cout << var << endl;
+
+    ++var;                  // Add just 1 to the variable and store it, can be written like var++ or var += 1 as well 
+    cout << var << endl;
 
     return 0;
 }
